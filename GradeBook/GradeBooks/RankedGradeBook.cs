f476ac2d9b3c9ae2c5dc929b85cc9858407b5cbf
Mateusz Biłka <mateusz.bilka@microsoft.wsei.edu.pl>
2023-03-20 14:14:13 +0100

    Overrode RankedGradeBook's CalculateStatistics method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -40,8 +40,17 @@ namespace GradeBook.GradeBooks
                 return 'D';
             else
                 return 'F';
+        }
 
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
 
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+
+            else
+
+                base.CalculateStatistics();
         }
     }
 }

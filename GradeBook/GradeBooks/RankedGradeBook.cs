bef4c3a6aeefb87b8cb85ac16df1d0425776c145
Mateusz Biłka <mateusz.bilka@microsoft.wsei.edu.pl>
2023-03-20 13:13:27 +0100

    Created the RankedGradeBook class and constructor

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name) 
+        {
+            base.Type = Enums.GradeBookType.Ranked;
+        
+        }
+    }
+}

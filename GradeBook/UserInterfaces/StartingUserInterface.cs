f062536696f0f31a8eb0e328862f39a53d14ea32
Mateusz Biłka <mateusz.bilka@microsoft.wsei.edu.pl>
2023-03-20 16:23:37 +0100

    Updated StartingUserInterface.CreateCommand

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -35,17 +35,31 @@ namespace GradeBook.UserInterfaces
         public static void CreateCommand(string command)
         {
             var parts = command.Split(' ');
-            if (parts.Length != 3)
+            if (parts.Length != 4)
             {
-                Console.WriteLine("Command not valid, Create requires a name and type of gradebook.");
+                Console.WriteLine("Command not valid, Create requires a name, type of gradebook, if it's weighted (true / false).");
                 return;
             }
+
+            
+
+
             var name = parts[1];
             var type = parts[2];
 
             bool isWeighted = false;
 
             BaseGradeBook gradeBook;
+
+            if (parts[3] == "true")
+            {
+                isWeighted = true;
+            }
+            else
+            {
+                isWeighted = false;
+            }
+
 
             if (type == "standard")
 
